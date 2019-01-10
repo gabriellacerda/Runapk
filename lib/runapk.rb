a87ba3cf4dac1baf34de890e7e6061e28924b597
GabrielLacerda <gabriellacerdadesign@gmail.com>
2019-01-10 16:05:44 -0300

    Added en compiler

--- lib/runapk.rb
+++ lib/runapk.rb
@@ -53,13 +53,14 @@ def compiler(doc, mode)
 
   path = prompt.ask("Localização do apk:")
 
-  phrase 'Agora informe a localização da sua keystore'.white
+  case mode
+  when 'prod'
+    phrase 'Agora informe a localização da sua keystore'.white
+    keystore_path = prompt.ask("Localização da keystore:")
 
-  keystore_path = prompt.ask("Localização da keystore:")
-
-  phrase "Iniciando processo de assinatura do apk"
-
-  cmd("(jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore #{keystore_path} #{path} alias_name)")
+    phrase "Iniciando processo de assinatura do apk"
+    cmd("(jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore #{keystore_path} #{path} alias_name)")
+  end
 
   phrase "Comprimindo app"
 
@@ -70,6 +71,60 @@ def compiler(doc, mode)
   phrase_simple ""
   phrase_simple "Obrigado por utilizar o RunApk!".green
 end
+
+def compiler_en(doc, mode)
+
+  name = doc.at_css("name").content
+  version = doc.at_css("widget").attribute('version')
+  phrase_simple 'App name: ' + name.blue
+  phrase_simple 'App version: ' + version
+  phrase_simple ""
+  app_name_export = name + '-' + version
+  app_name_export = app_name_export.gsub!(/\s/, '')
+  end_app_phrase = "Your apk will be called: ".white + "#{app_name_export}.apk".blue
+  phrase_simple end_app_phrase
+
+  case mode
+  when 'dev'
+    params = ''
+    prod = false
+  when 'prod'
+    params = '--prod --release'
+    prod = true
+  end
+
+  phrase 'Starting the build process'
+
+  cmd("(ionic cordova build android #{params})")
+
+  phrase 'Your apk has been build, however to continue you need to inform the apk directory, it is above, just copy it and paste it below'.white
+
+  phrase_simple 'It looks like this: Built the following apk (s): /Users/apk/path/apk.apk, copy that line and paste it below'.green
+  phrase_simple ""
+
+  prompt = TTY::Prompt.new
+
+  path = prompt.ask("Apk location:")
+
+  case mode
+  when 'prod'
+    phrase 'Now enter the location of your keystore'.white
+    keystore_path = prompt.ask("Keystore location:")
+
+    phrase "Starting the apk signing process"
+    cmd("(jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore #{keystore_path} #{path} alias_name)")
+  end
+
+  phrase "Compressing app"
+
+  cmd("(zipalign  -v 4 #{path} #{app_name_export}.apk)")
+  phrase_simple ""
+  phrase_simple "Your app is ready!"
+  phrase_simple "Just browse the root folder of your ionic project"
+  phrase_simple ""
+  phrase_simple "Thanks for using RunApk!".green
+end
+
 
 def setup()
   case ARGV[0]
@@ -94,7 +149,7 @@ def setup()
     phrase_simple ""
     phrase_simple '👩‍🚀  Brr Dr, ei onde está o meu comp... 👩‍🚀'.white
     phrase_simple ""
-    platform = prompt.select("👩‍🚀 Para qual plataform você deseja exportar?", %w(Android iOS))
+    platform = prompt.select("👩‍🚀 Para qual plataform você deseja exportar?", %w(Android))
     phrase_simple ""
     type = prompt.select("👩‍🚀 Que tipo de apk você deseja?", ['Desenvolvimento (Build rápida, recomendado para testes locais)', 'Produção (Inclui todas as otimizações necessárias para exportar o app para a Play Store ou Apple Store) Se for para android, você vai precisar de uma keystore: https://runapk.page.link/keystore'])
     phrase_simple ""
@@ -104,13 +159,32 @@ def setup()
     when 'É claro'
       @doc = Nokogiri::XML(File.open("config.xml"))
       case type
-      when 'Development'
+      when 'Desenvolvimento (Build rápida, recomendado para testes locais)'
         compiler  @doc, 'dev'
       else
         compiler @doc, 'prod'
       end
     end
   else
+    phrase_simple ""
+    phrase_simple '👩‍🚀  Brr Dr, where is my comp... 👩‍🚀'.white
+    phrase_simple ""
+    platform = prompt.select("👩‍🚀 For which plataform you want to export?", %w(Android))
+    phrase_simple ""
+    type = prompt.select("👩‍🚀 What kind of apk do you want?", ["Development (Fast build, recommended for local testing)", "Production (Includes all the optimizations needed to export the app to the Play Store or Apple Store) If it's for android, you'll need a keystore: https://runapk.page.link/keystore"])
+    phrase_simple ""
+    load_config = prompt.select("👩‍🚀 I don't know the name and version of your app, do you want me to load them from your configuration file? It's the config.xml file by the way ...", ['Sure'])
+    phrase_simple ""
+    case load_config
+    when 'Sure'
+      @doc = Nokogiri::XML(File.open("config.xml"))
+      case type
+      when 'Development (Fast build, recommended for local testing)'
+        compiler_en  @doc, 'dev'
+      else
+        compiler_en @doc, 'prod'
+      end
+    end
   end
 end
 
